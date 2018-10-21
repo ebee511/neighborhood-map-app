90b05e8030d7957cd5c2fb7b8b425c0eeccce5ad
Erica Bee <ebee511@gmail.com>
2018-10-20 22:55:45 -0700

    Add sw file.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
+// import registerServiceWorker from './registerServiceWorker';
+import startServiceWorker from './startServiceWorker.js';
 
 ReactDOM.render(
 		<App />, 
 		document.getElementById('root')
 );
 
-registerServiceWorker();
+startServiceWorker();
