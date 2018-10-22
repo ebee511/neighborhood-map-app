7b4404524719f0b682722d99b43577d46d20cbac
Erica Bee <ebee511@gmail.com>
2018-10-21 19:05:20 -0700

    Add keypress functionality for list items.

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import $ from 'jquery';
 
 const ListItem = (props) => {
 	return (
 		<li 
 			tabIndex={0}
 			aria-label={props.name}
+			role='button'
 			className='listItem' 
 			onClick={() => props.handleListItemClick(props)}
+			onKeyPress={() => props.handleListItemClick(props)}
 		>
 			{props.name}
 		</li>
