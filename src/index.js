103447bebd056bc972d1509f4b59eb5175b1bc63
Erica Bee <ebee511@gmail.com>
2018-10-21 19:01:34 -0700

    Include jquery.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 import startServiceWorker from './startServiceWorker.js';
+import $ from 'jquery';
 
 ReactDOM.render(
 		<App />, 
